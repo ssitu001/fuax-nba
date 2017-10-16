368a626047305720d4a8fda73cbdb3a6e8b787dd
Simon Situ <ssitu001@gmail.com>
2017-10-16 16:32:36 -0700

    initial project setup

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.css';
+
+const Header = () => (
+  <header className="Header-header">
+    <img src='./images/logo.png' className="Header-logo" alt="" />
+  </header>
+);
+
+export default Header;
+
+
