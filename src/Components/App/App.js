dafaef536a76a7ad1bd6668223f359534b9164e5
Simon Situ <ssitu001@gmail.com>
2017-10-19 14:38:21 -0700

    add react router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
 
 import Header from '../Header/Header';
+import Home from '../Home/Home';
+import Teams from '../Teams/Teams';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+    <BrowserRouter>
       <div>
         <Header />
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/teams" component={Teams}></Route>
       </div>
+    </BrowserRouter>
     );
   }
 }
