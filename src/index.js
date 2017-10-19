0e5e68f8b6a2e7380de3e27268dc77df26e29f5c
Simon Situ <ssitu001@gmail.com>
2017-10-19 14:37:55 -0700

    add react router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
 
 import './index.css';
 
 import App from './Components/App/App';
-import Teams from './Components/Teams/Teams';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <div>
-      <Route exact path="/" component={App}></Route>
-      <Route path="/teams" component={Teams}></Route>
-    </div>
-  </BrowserRouter>
-  , document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
